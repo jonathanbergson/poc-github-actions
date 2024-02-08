7a38dbed363d4b9bc8258f411c0c626488c89933
Jonathan Bergson <contato@bergson.me>
2024-02-08 15:17:22 -0300

    ahhhhhhhh

--- .github/workflows/jira.js
+++ .github/workflows/jira.js
@@ -15,16 +15,17 @@ const getJiraCardNumberWithPrefix = (prTitle) => {
   return prefix + getJiraCardNumber(prTitle);
 }
 
-const pullRequestCreated = () => {
+const pullRequestCreated = (prId) => {
   const doc = new Document();
 
   doc
     .panel('info')
     .paragraph()
-    .text('Pull Request criado: \n')
+    .text('Pull Request criado:')
+    .paragraph()
     .text('Assignees: @jonathanbergson \n')
     .text('Link: ')
-    .link('https://github.com/Tracksale/cxm-app/pull/1507', 'https://github.com/Tracksale/cxm-app/pull/1507')
+    .link(`https://github.com/Tracksale/cxm-app/pull/${prId}`, `https://github.com/Tracksale/cxm-app/pull/${prId}`)
 
   return doc.toJSON();
 };
