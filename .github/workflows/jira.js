8756590487e11ac805fee869ba75e9d5348b063c
Jonathan Bergson <contato@bergson.me>
2024-02-08 10:50:55 -0300

    ahhhhhhhh

--- .github/workflows/jira.js
+++ .github/workflows/jira.js
@@ -1,35 +1,15 @@
-var { Document } = require('adf-builder');
+const {Document} = require('adf-builder');
 
 const pullRequestCreated = () => {
   const doc = new Document();
 
-
-  doc.panel("info")
+  doc
+    .panel('info')
     .paragraph()
-    .text("Pull Request criado: \n")
-    .text("Assignees: @jonathanbergson \n")
-    .text("Link: ")
-    .link("https://github.com/Tracksale/cxm-app/pull/1507")
-
-  // doc.paragraph()
-  //   .text('Here is some ')
-  //   .strong('bold test')
-  //   .text(' and ')
-  //   .em('text in italics')
-  //   .text(' as well as ')
-  //   .link(' a link', 'https://www.atlassian.com')
-  //   .text(' , emojis ')
-  //   .emoji(':smile:')
-  //   .emoji(':rofl:')
-  //   .emoji(':nerd:')
-  //   .text(' and some code: ')
-  //   .code('var i = 0;')
-  //   .text(' and a bullet list');
-  // doc.bulletList()
-  //   .textItem('With one bullet point')
-  //   .textItem('And another');
-  // doc.codeBlock("javascript")
-  //   .text('var i = 0;\nwhile(true) {\n  i++;\n}');
+    .text('Pull Request criado: \n')
+    .text('Assignees: @jonathanbergson \n')
+    .text('Link: ')
+    .link('https://github.com/Tracksale/cxm-app/pull/1507',  'https://github.com/Tracksale/cxm-app/pull/1507')
 
   return doc.toJSON();
 };
@@ -37,61 +17,3 @@ const pullRequestCreated = () => {
 module.exports = {
   pullRequestCreated,
 };
-
-
-const body = {
-  "body": {
-    "type": "doc",
-      "version": 1,
-      "content": [
-      {
-        "type": "panel",
-        "attrs": {
-          "panelType": "info"
-        },
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": "Pull Request criado:"
-              }
-            ]
-          },
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": "Assignees: @jonathanbergson"
-              }
-            ]
-          },
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": "Link: "
-              },
-              {
-                "type": "text",
-                "text": "https://github.com/Tracksale/cxm-app/pull/1507",
-                "marks": [
-                  {
-                    "type": "link",
-                    "attrs": {
-                      "href": "https://github.com/Tracksale/cxm-app/pull/1507",
-                      "title": "Pull Request link"
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-}
